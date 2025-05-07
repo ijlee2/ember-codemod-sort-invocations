e984aba612f9042630755d64a537c068a6fbebf6
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-05-07 08:22:53 +0200

    refactor: Added tests (sort-hash)

---
+++ tests/utils/sort-invocations/sort-hash/sort.test.ts
@@ -0,0 +1,39 @@
+import { AST } from '@codemod-utils/ast-template';
+import { assert, test } from '@codemod-utils/tests';
+
+import { sortHash } from '../../../../src/utils/sort-invocations/sort-hash.js';
+
+test('utils | sort-invocations | sort-hash > sort', function () {
+  const oldFile = [
+    `{{t`,
+    `  "my-component.description"`,
+    `  packageVersion="6.0.0"`,
+    `  packageName="@ember/source"`,
+    `  installedOn=this.installationDate`,
+    `}}`,
+  ].join('\n');
+
+  const traverse = AST.traverse();
+
+  const ast = traverse(oldFile, {
+    MustacheStatement(node) {
+      const { hash } = node;
+
+      node.hash = sortHash(hash);
+    },
+  });
+
+  const newFile = AST.print(ast);
+
+  assert.strictEqual(
+    newFile,
+    [
+      `{{t`,
+      `  "my-component.description"`,
+      `  installedOn=this.installationDate`,
+      `  packageName="@ember/source"`,
+      `  packageVersion="6.0.0"`,
+      `}}`,
+    ].join('\n'),
+  );
+});
