4673898245dc279fed36113875d26d0a8fafdf09
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-05-09 07:51:44 +0200

    chore: Addressed breaking changes in content-tag@4.0.0

--- src/utils/ast/template-tag.ts
+++ src/utils/ast/template-tag.ts
@@ -1,18 +1,44 @@
 import { Preprocessor } from 'content-tag';
 
+const BufferMap = new Map<string, ArrayBuffer>();
+
+function getBuffer(str: string): ArrayBuffer {
+  let buffer = BufferMap.get(str);
+
+  if (!buffer) {
+    buffer = Buffer.from(str);
+    BufferMap.set(str, buffer);
+  }
+
+  return buffer;
+}
+
+function sliceByteRange(
+  str: string,
+  indexStart: number,
+  indexEnd?: number,
+): string {
+  const buffer = getBuffer(str);
+
+  // eslint-disable-next-line @typescript-eslint/no-base-to-string
+  return buffer.slice(indexStart, indexEnd).toString();
+}
+
 type Range = {
-  end: number;
-  start: number;
+  endByte: number;
+  endChar: number;
+  startByte: number;
+  startChar: number;
 };
 
 type ContentTag = {
   contentRange: Range;
   contents: string;
   endRange: Range;
-  range: Range; // range = startRange + contentRange + endRange
+  range: Range;
   startRange: Range;
   tagName: string;
-  type: string;
+  type: 'class-member' | 'expression';
 };
 
 export function parse(file: string) {
@@ -31,10 +57,10 @@ export function replaceTemplate(
   const { contents, range } = options;
 
   return [
-    file.substring(0, range.start),
+    sliceByteRange(file, 0, range.startByte),
     '<template>',
     contents,
     '</template>',
-    file.substring(range.end),
+    sliceByteRange(file, range.endByte, undefined),
   ].join('');
 }
